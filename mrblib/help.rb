d69886f2e800851965091d83d05ddd70ac3487e2
Zachary Scott <e@zzak.io>
2015-07-24 14:39:20 -0400

    Brackets might imply optional values, but these are required for setup

--- mrblib/help.rb
+++ mrblib/help.rb
@@ -7,7 +7,7 @@ module MrubyCli
     def run
       @output_io.puts "mruby-cli [switches] [arguments]"
       @output_io.puts "mruby-cli -h, --help               : show this message"
-      @output_io.puts "mruby-cli -s[name], --setup[=name] : setup your app"
+      @output_io.puts "mruby-cli -s<name>, --setup=<name> : setup your app"
       @output_io.puts "mruby-cli -v, --version            : print mruby-cli version"
     end
   end

74759a73baf1bee49895c1bc4e08e4b6557daaa8
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2015-11-05 01:15:44 +0100

    Fix linking/compiling issues with mruby-docopt for linux and osx

--- build_config.rb
+++ build_config.rb
@@ -11,6 +11,7 @@ MRuby::Build.new do |conf|
   conf.enable_bintest
   conf.enable_debug
   conf.enable_test
+  conf.enable_cxx_abi
 
   gem_config(conf)
 end
@@ -18,6 +19,7 @@ end
 MRuby::Build.new('x86_64-pc-linux-gnu') do |conf|
   toolchain :gcc
 
+  conf.enable_cxx_abi
   gem_config(conf)
 end
 
@@ -28,67 +30,76 @@ MRuby::CrossBuild.new('i686-pc-linux-gnu') do |conf|
     cc.flags << "-m32"
   end
 
+  conf.enable_cxx_abi
   gem_config(conf)
 end
 
-#MRuby::CrossBuild.new('x86_64-apple-darwin14') do |conf|
-#  toolchain :clang
-#
-#  [conf.cc, conf.linker].each do |cc|
-#    cc.command = 'x86_64-apple-darwin14-clang'
-#  end
-#  conf.cxx.command      = 'x86_64-apple-darwin14-clang++'
-#  conf.archiver.command = 'x86_64-apple-darwin14-ar'
-#
-#  conf.build_target     = 'x86_64-pc-linux-gnu'
-#  conf.host_target      = 'x86_64-apple-darwin14'
-#
-#  gem_config(conf)
-#end
-#
-#MRuby::CrossBuild.new('i386-apple-darwin14') do |conf|
-#  toolchain :clang
-#
-#  [conf.cc, conf.linker].each do |cc|
-#    cc.command = 'i386-apple-darwin14-clang'
-#  end
-#  conf.cxx.command      = 'i386-apple-darwin14-clang++'
-#  conf.archiver.command = 'i386-apple-darwin14-ar'
-#
-#  conf.build_target     = 'i386-pc-linux-gnu'
-#  conf.host_target      = 'i386-apple-darwin14'
-#
-#  gem_config(conf)
-#end
-#
-#MRuby::CrossBuild.new('x86_64-w64-mingw32') do |conf|
+MRuby::CrossBuild.new('x86_64-apple-darwin14') do |conf|
+ toolchain :clang
+
+ [conf.cc, conf.linker].each do |cc|
+   cc.command = 'x86_64-apple-darwin14-clang'
+ end
+ conf.cxx.command      = 'x86_64-apple-darwin14-clang++'
+ conf.cxx.flags << "-std=c++11 -stdlib=libc++"
+ conf.linker.flags << "-std=c++11 -stdlib=libc++"
+ conf.archiver.command = 'x86_64-apple-darwin14-ar'
+
+ conf.build_target     = 'x86_64-pc-linux-gnu'
+ conf.host_target      = 'x86_64-apple-darwin14'
+
+ conf.enable_cxx_abi
+ gem_config(conf)
+end
+
+MRuby::CrossBuild.new('i386-apple-darwin14') do |conf|
+ toolchain :clang
+
+ [conf.cc, conf.linker].each do |cc|
+   cc.command = 'i386-apple-darwin14-clang'
+ end
+ conf.cxx.command      = 'i386-apple-darwin14-clang++'
+ conf.cxx.flags << "-std=c++11 -stdlib=libc++"
+ conf.linker.flags << "-std=c++11 -stdlib=libc++"
+ conf.archiver.command = 'i386-apple-darwin14-ar'
+
+ conf.build_target     = 'i386-pc-linux-gnu'
+ conf.host_target      = 'i386-apple-darwin14'
+
+ conf.enable_cxx_abi
+ gem_config(conf)
+end
+
+# MRuby::CrossBuild.new('x86_64-w64-mingw32') do |conf|
 #  toolchain :gcc
-#
+
 #  [conf.cc, conf.linker].each do |cc|
 #    cc.command = 'x86_64-w64-mingw32-gcc'
 #  end
 #  conf.cxx.command      = 'x86_64-w64-mingw32-cpp'
 #  conf.archiver.command = 'x86_64-w64-mingw32-gcc-ar'
 #  conf.exts.executable  = ".exe"
-#
+
 #  conf.build_target     = 'x86_64-pc-linux-gnu'
 #  conf.host_target      = 'x86_64-w64-mingw32'
-#
+
+#  conf.enable_cxx_abi
 #  gem_config(conf)
-#end
-#
-#MRuby::CrossBuild.new('i686-w64-mingw32') do |conf|
+# end
+
+# MRuby::CrossBuild.new('i686-w64-mingw32') do |conf|
 #  toolchain :gcc
-#
+
 #  [conf.cc, conf.linker].each do |cc|
 #    cc.command = 'i686-w64-mingw32-gcc'
 #  end
 #  conf.cxx.command      = 'i686-w64-mingw32-cpp'
 #  conf.archiver.command = 'i686-w64-mingw32-gcc-ar'
 #  conf.exts.executable  = ".exe"
-#
+
 #  conf.build_target     = 'i686-pc-linux-gnu'
 #  conf.host_target      = 'i686-w64-mingw32'
-#
+
+#  conf.enable_cxx_abi
 #  gem_config(conf)
-#end
+# end

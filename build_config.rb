a00f779bed651c7e2efae6b352f875321b5b00d7
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2015-11-05 02:17:16 +0100

    Enable win targets

--- build_config.rb
+++ build_config.rb
@@ -70,36 +70,36 @@ MRuby::CrossBuild.new('i386-apple-darwin14') do |conf|
  gem_config(conf)
 end
 
-# MRuby::CrossBuild.new('x86_64-w64-mingw32') do |conf|
-#  toolchain :gcc
+MRuby::CrossBuild.new('x86_64-w64-mingw32') do |conf|
+ toolchain :gcc
 
-#  [conf.cc, conf.linker].each do |cc|
-#    cc.command = 'x86_64-w64-mingw32-gcc'
-#  end
-#  conf.cxx.command      = 'x86_64-w64-mingw32-cpp'
-#  conf.archiver.command = 'x86_64-w64-mingw32-gcc-ar'
-#  conf.exts.executable  = ".exe"
+ [conf.cc, conf.linker].each do |cc|
+   cc.command = 'x86_64-w64-mingw32-gcc'
+ end
+ conf.cxx.command      = 'x86_64-w64-mingw32-g++'
+ conf.archiver.command = 'x86_64-w64-mingw32-gcc-ar'
+ conf.exts.executable  = ".exe"
 
-#  conf.build_target     = 'x86_64-pc-linux-gnu'
-#  conf.host_target      = 'x86_64-w64-mingw32'
+ conf.build_target     = 'x86_64-pc-linux-gnu'
+ conf.host_target      = 'x86_64-w64-mingw32'
 
-#  conf.enable_cxx_abi
-#  gem_config(conf)
-# end
+ conf.enable_cxx_abi
+ gem_config(conf)
+end
 
-# MRuby::CrossBuild.new('i686-w64-mingw32') do |conf|
-#  toolchain :gcc
+MRuby::CrossBuild.new('i686-w64-mingw32') do |conf|
+ toolchain :gcc
 
-#  [conf.cc, conf.linker].each do |cc|
-#    cc.command = 'i686-w64-mingw32-gcc'
-#  end
-#  conf.cxx.command      = 'i686-w64-mingw32-cpp'
-#  conf.archiver.command = 'i686-w64-mingw32-gcc-ar'
-#  conf.exts.executable  = ".exe"
+ [conf.cc, conf.linker].each do |cc|
+   cc.command = 'i686-w64-mingw32-gcc'
+ end
+ conf.cxx.command      = 'i686-w64-mingw32-g++'
+ conf.archiver.command = 'i686-w64-mingw32-gcc-ar'
+ conf.exts.executable  = ".exe"
 
-#  conf.build_target     = 'i686-pc-linux-gnu'
-#  conf.host_target      = 'i686-w64-mingw32'
+ conf.build_target     = 'i686-pc-linux-gnu'
+ conf.host_target      = 'i686-w64-mingw32'
 
-#  conf.enable_cxx_abi
-#  gem_config(conf)
-# end
+ conf.enable_cxx_abi
+ gem_config(conf)
+end

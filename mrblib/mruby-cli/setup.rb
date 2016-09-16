247e06280e20a76d8c0f8e8d48e1782aba64c0a9
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-09-16 12:59:26 +0900

    Not to chdir globally * Calling Dir.chdir in top level of Rakefile makes us not to add any tasks which should run on project root directory.

--- mrblib/mruby-cli/setup.rb
+++ mrblib/mruby-cli/setup.rb
@@ -330,9 +330,7 @@ mruby_config=File.expand_path(ENV["MRUBY_CONFIG"] || "build_config.rb")
 ENV['MRUBY_ROOT'] = mruby_root
 ENV['MRUBY_CONFIG'] = mruby_config
 Rake::Task[:mruby].invoke unless Dir.exist?(mruby_root)
-Dir.chdir(mruby_root)
 load "\#{mruby_root}/Rakefile"
-
 
 load File.join(File.expand_path(File.dirname(__FILE__)), "mrbgem.rake")
 
@@ -342,13 +340,14 @@ APP_VERSION = (app_version.nil? || app_version.empty?) ? "unknown" : app_version
 
 desc "compile binary"
 task :compile => [:all] do
-
-  MRuby.each_target do |target|
-    `\#{target.cc.command} --version`
-    abort("Command \#{target.cc.command} for \#{target.name} is missing.") unless $?.success?
-  end
-  %W(\#{mruby_root}/build/x86_64-pc-linux-gnu/bin/\#{APP_NAME} \#{mruby_root}/build/i686-pc-linux-gnu/\#{APP_NAME}).each do |bin|
-    sh "strip --strip-unneeded \#{bin}" if File.exist?(bin)
+  Dir.chdir(mruby_root) do
+    MRuby.each_target do |target|
+      `\#{target.cc.command} --version`
+      abort("Command \#{target.cc.command} for \#{target.name} is missing.") unless $?.success?
+    end
+    %W(\#{mruby_root}/build/x86_64-pc-linux-gnu/bin/\#{APP_NAME} \#{mruby_root}/build/i686-pc-linux-gnu/\#{APP_NAME}).each do |bin|
+      sh "strip --strip-unneeded \#{bin}" if File.exist?(bin)
+    end
   end
 end
 
@@ -356,12 +355,14 @@ namespace :test do
   desc "run mruby & unit tests"
   # only build mtest for host
   task :mtest => :compile do
-    # in order to get mruby/test/t/synatx.rb __FILE__ to pass,
-    # we need to make sure the tests are built relative from mruby_root
-    MRuby.each_target do |target|
-      # only run unit tests here
-      target.enable_bintest = false
-      run_test if target.test_enabled?
+    Dir.chdir(mruby_root) do
+      # in order to get mruby/test/t/synatx.rb __FILE__ to pass,
+      # we need to make sure the tests are built relative from mruby_root
+      MRuby.each_target do |target|
+        # only run unit tests here
+        target.enable_bintest = false
+        run_test if target.test_enabled?
+      end
     end
   end
 
@@ -379,9 +380,11 @@ namespace :test do
 
   desc "run integration tests"
   task :bintest => :compile do
-    MRuby.each_target do |target|
-      clean_env(%w(MRUBY_ROOT MRUBY_CONFIG)) do
-        run_bintest if target.bintest_enabled?
+    Dir.chdir(mruby_root) do
+      MRuby.each_target do |target|
+        clean_env(%w(MRUBY_ROOT MRUBY_CONFIG)) do
+          run_bintest if target.bintest_enabled?
+        end
       end
     end
   end
@@ -393,7 +396,9 @@ task :test => ["test:mtest", "test:bintest"]
 
 desc "cleanup"
 task :clean do
-  sh "rake deep_clean"
+  Dir.chdir(mruby_root) do
+    sh "rake deep_clean"
+  end
 end
 
 desc "generate a release tarball"
